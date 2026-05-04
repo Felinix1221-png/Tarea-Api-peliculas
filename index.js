72fa831ce90882da9369acca061a1f34756fc7ca
Giancarlo Felix Rojo <g.felix24@info.uas.edu.mx>
2026-05-03 17:55:17 -0700

    actualizacion token

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const sequelize = require('./models/database');
+const sequelize = require('./database');
 const peliculasRoutes = require('./routes/peliculas.routes');
+const authRoutes = require('./routes/auth.routes'); // Importamos las rutas de login
 const logger = require('./middlewares/logger');
-const validarApiKey = require('./middlewares/validarApiKey');
+const validarToken = require('./middlewares/validarToken'); // Importamos el middleware JWT
 
 const app = express();
 const PORT = 3000;
 
 // Middlewares globales
-app.use(express.json()); // Para parsear el body en JSON
-app.use(logger);         // Middleware de registro
-
-// Rutas (Aplicando el middleware de API Key a todas las rutas de /peliculas)
-app.use('/peliculas', validarApiKey, peliculasRoutes);
+app.use(express.json()); 
+app.use(logger);         
+app.use('/auth', authRoutes);
+app.use('/peliculas', validarToken, peliculasRoutes);
 
 // Sincronizar base de datos e iniciar servidor
-sequelize.sync({ force: false }) //false evita borrar los datos al reiniciar
+sequelize.sync({ force: false }) // false evita borrar los datos al reiniciar
     .then(() => {
         console.log('Base de datos SQLite sincronizada.');
         app.listen(PORT, () => {
